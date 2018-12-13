9a28f704a57039f2e9ef4270185a2c3509391e1a
Anton Efimenko <reloni@ya.ru>
2018-12-13 15:06:45 +0300

    Initial Commit

---
+++ GoogleMusicClient/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GoogleMusicClient
+//
+//  Created by Anton Efimenko on 13/12/2018.
+//  Copyright © 2018 Anton Efimenko. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GoogleMusicClient/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GoogleMusicClient
+//
+//  Created by Anton Efimenko on 13/12/2018.
+//  Copyright © 2018 Anton Efimenko. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
