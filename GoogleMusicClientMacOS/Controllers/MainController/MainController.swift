13fca22aa4885d427320d0058fa9e1755b99589a
Anton Efimenko <reloni@ya.ru>
2018-12-17 16:28:01 +0300

    Load station feed on table tap

--- GoogleMusicClientMacOS/Controllers/MainController/MainController.swift
+++ GoogleMusicClientMacOS/Controllers/MainController/MainController.swift
@@ -8,6 +8,8 @@
 
 import Cocoa
 import RxDataFlow
+import RxSwift
+import RxCocoa
 import RxGoogleMusic
 
 final class MainController: NSViewController {
@@ -15,9 +17,28 @@ final class MainController: NSViewController {
         return GMusicClient(token: Global.current.gMusicToken!)
     }
     
+    let bag = DisposeBag()
+    
+    @IBOutlet weak var tableView: ApplicationTableView!
+    
+    var stations = GMusicCollection<GMusicRadioStation>(kind: "") {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
     override func viewDidAppear() {
         super.viewDidAppear()
-        _ = client.radioStations().subscribe(onNext: { print($0.items.count) })
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        client
+            .radioStations()
+            .asDriver(onErrorJustReturn: stations)
+            .do(onNext: { [weak self] in self?.stations = $0 })
+            .drive()
+            .disposed(by: bag)
     }
     
     @IBAction func logOff(_ sender: Any) {
@@ -27,5 +48,27 @@ final class MainController: NSViewController {
     
     deinit {
         print("MainController deinit")
+    }
+}
+
+extension MainController: NSTableViewDataSource {
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return stations.items.count
+    }
+}
+
+extension MainController: NSTableViewDelegate {
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "Cell"), owner: nil) as! NSTableCellView
+        cell.textField?.stringValue = stations.items[row].name
+        return cell
+    }
+    
+    func tableViewSelectionDidChange(_ notification: Notification) {
+        let station = stations.items[tableView.selectedRow]
+        client.radioStationFeed(for: station)
+            .do(onNext: { result in print(result.items.first!.tracks.map { "\($0.id?.uuidString ?? "") | \($0.title)" }) })
+            .subscribe()
+            .disposed(by: bag)
     }
 }
---
+++ GoogleMusicClientMacOS/Core/Views/ApplicationTableView.swift
@@ -0,0 +1,23 @@
+//
+//  ApplicationTableView.swift
+//  GoogleMusicClientMacOS
+//
+//  Created by Anton Efimenko on 17/12/2018.
+//  Copyright © 2018 Anton Efimenko. All rights reserved.
+//
+
+import Cocoa
+
+final class ApplicationTableView: NSTableView {
+    override func drawGrid(inClipRect clipRect: NSRect) { }
+    override func drawBackground(inClipRect clipRect: NSRect) { }
+    override func reloadData() {
+        if numberOfRows > 0 {
+            removeRows(at: IndexSet(0..<numberOfRows), withAnimation: NSTableView.AnimationOptions.effectFade)
+        }
+        
+        guard let rows = dataSource?.numberOfRows?(in: self), rows > 0 else { return }
+        
+        insertRows(at: IndexSet(0..<rows), withAnimation: NSTableView.AnimationOptions.slideDown)
+    }
+}
