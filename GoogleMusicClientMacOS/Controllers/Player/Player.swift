258706791aa4191099855f8231796717fcbbb51d
Anton Efimenko <reloni@ya.ru>
2018-12-26 13:02:52 +0300

    Play song with new player

--- GoogleMusicClientMacOS/Controllers/Player/Player.swift
+++ GoogleMusicClientMacOS/Controllers/Player/Player.swift
@@ -21,15 +21,6 @@ private func writeData(_ data: Data, to url: URL) throws {
     try data.write(to: url)
 }
 
-private func newTrackFile() -> URL {
-    return Global.current.musicDirectory.appendingPathComponent("\(UUID().uuidString).aac")
-}
-
-private func download(track: GMusicTrack, using client: GMusicClient) -> Single<Data> {
-    guard let id = (track.nid ?? track.id?.uuidString) else { fatalError("No nid and id ¯\\_(ツ)_/¯") }
-    return client.downloadTrack(id: id)
-}
-
 private func saveTrack(to path: URL) -> (Single<Data>) -> Single<URL> {
     return { data in
         return data.flatMap { data in try writeData(data, to: path); return .just(path) }
@@ -37,20 +28,70 @@ private func saveTrack(to path: URL) -> (Single<Data>) -> Single<URL> {
 }
 
 private func playTrack(with player: AVPlayer) -> (Single<URL>) -> Completable {
-    return { url in
-        return .empty()
+    return { request in
+        return request.flatMapCompletable { [weak player] file in
+            guard let p = player else { return .empty() }
+            setAsset(from: file, with: p)
+            p.rate = 1.0
+            return .empty()
+        }
+    }
+}
+
+private func setAsset(from file: URL, with player: AVPlayer) {
+    let asset = AVURLAsset(url: file)
+    let item = AVPlayerItem(asset: asset)
+    player.replaceCurrentItem(with: item)
+}
+
+extension AVPlayer {
+    var currentItemTime: Observable<Double?> {
+        return Observable<Int>.timer(1, scheduler: MainScheduler.instance).flatMap { [weak player = self] _ -> Observable<Double?> in
+            return .just(player?.currentTime().seconds)
+        }
+    }
+    
+    var currentItemStatus: Observable<Player.Status?> {
+        return Observable<Int>.timer(1, scheduler: MainScheduler.instance).flatMap { [weak player = self] _ -> Observable<Player.Status?>  in
+            guard let i = player?.currentItem else { return .just(nil) }
+            return .just(Player.Status(raw: i.status))
+        }.distinctUntilChanged()
+    }
+}
+
+extension GMusicTrack {
+    var identifier: String {
+        return nid ?? id?.uuidString ?? storeId ?? ""
     }
 }
 
 final class Player {
+    enum Status {
+        case failed
+        case readyToPlay
+        case unknown
+        
+        init(raw: AVPlayerItem.Status) {
+            switch raw {
+            case .failed: self = .failed
+            case .readyToPlay: self = .readyToPlay
+            case .unknown: self = .unknown
+            }
+        }
+    }
     
-    private let bag = DisposeBag()
-    
-    private let queue: [GMusicTrack]
+    private var queue: [GMusicTrack]
     private let avPlayer: AVPlayer
     private var currentTrackIndex: Int = -1
     private let rootPath: URL
     private let loadRequest: (GMusicTrack) -> Single<Data>
+    
+    private let currentTrackSubject = BehaviorSubject<GMusicTrack?>(value: nil)
+    var currentTrack: Observable<GMusicTrack?> {
+        return currentTrackSubject.distinctUntilChanged { $0?.identifier == $1?.identifier }
+    }
+    var currentItemTime: Observable<Double?> { return avPlayer.currentItemTime }
+    var currentItemStatus: Observable<Player.Status?> { return avPlayer.currentItemStatus }
     
     init(rootPath: URL, loadRequest: @escaping (GMusicTrack) -> Single<Data>, queue: [GMusicTrack]) {
         self.rootPath = rootPath
@@ -61,14 +102,22 @@ final class Player {
     
     func playNext() -> Completable {
         currentTrackIndex += 1
-        guard currentTrackIndex < queue.count else { return Completable.empty() }
-        return play(track: queue[currentTrackIndex])
-    }
-    
-    private func play(track: GMusicTrack) -> Completable {
+        guard currentTrackIndex < queue.count else {
+            currentTrackSubject.onNext(nil)
+            return .empty()
+        }
+        
+        let track = queue[currentTrackIndex]
+        currentTrackSubject.onNext(track)
+        
         return track
             |> loadRequest
             >>> saveTrack(to: rootPath.randomAac)
             >>> playTrack(with: avPlayer)
+    }
+    
+    func resetQueue(new items: [GMusicTrack]) {
+        currentTrackIndex = -1
+        queue = items
     }
 }
--- GoogleMusicClientMacOS/Controllers/Player/PlayerController.swift
+++ GoogleMusicClientMacOS/Controllers/Player/PlayerController.swift
@@ -9,6 +9,7 @@
 import Cocoa
 import RxGoogleMusic
 import RxSwift
+import RxCocoa
 import AVFoundation
 
 final class PlayerController: NSViewController {
@@ -30,60 +31,44 @@ final class PlayerController: NSViewController {
     @IBOutlet weak var songProgressIndication: NSProgressIndicator!
     @IBOutlet weak var volumeSlider: NSSlider!
     
-    var asset: AVURLAsset? = nil
-    var item: AVPlayerItem? = nil
-    var player: AVPlayer? = nil
+    let player = Player(rootPath: Global.current.musicDirectory,
+                        loadRequest: Global.current.dataFlowController.currentState.state.client!.downloadTrack,
+                        queue: [])
     
     let bag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        Global.current.dataFlowController.state.observeOn(MainScheduler.instance).do(onNext: { result in
-            if case PlayerAction.loadRadioStationFeed = result.setBy {
-                self.update()
-            }
-        })
-        .subscribe()
-        .disposed(by: bag)
+        bind()
     }
     
-    func update() {
-        guard let track = Global.current.dataFlowController.currentState.state.tracks.first else { return }
-        songTitleLabel.stringValue = track.title
-        artistAndAlbumLabel.stringValue = "\(track.album) (\(track.artist))"
+    func bind() {
+        Global.current.dataFlowController.state.observeOn(MainScheduler.instance).do(onNext: { [weak self] result in
+            if case PlayerAction.loadRadioStationFeed = result.setBy { self?.resetQueue() }
+        }) .subscribe().disposed(by: bag)
         
-//        print(Global.current.dataFlowController.currentState.state.tracks.map { "nid: \($0.nid ?? "") title: \($0.title)" })
-//
-//        let music = getMusicDirectory()
-//
-//        let client = Global.current.dataFlowController.currentState.state.client!
-//        guard let nid = track.nid else { return }
-//        client.downloadTrack(id: nid)
-//            .do(onSuccess: { [weak self] data in try self?.saveAndPlay(data, to: music.appendingPathComponent("\(UUID().uuidString).aac")) })
-//            .do(onError: { print("error: \($0)") })
-//            .subscribe()
-//            .disposed(by: bag)
+        playPauseButon.rx.tap
+            .flatMap { [weak player] in player?.playNext().asObservable() ?? .empty() }
+            .do(onError: PlayerController.onError)
+            .retry()
+            .subscribe()
+            .disposed(by: bag)
     }
     
-    func saveAndPlay(_ data: Data, to url: URL) throws {
-        print(url)
-        try data.write(to: url)
-        
-        asset = AVURLAsset(url: url)
-        item = AVPlayerItem(asset: asset!)
-        
-        player = AVPlayer(playerItem: item!)
-        player?.volume = 1.0
-        player?.rate = 1.0
-        
-//        self.player!.addPeriodicTimeObserver(forInterval: CMTime(seconds: 1, preferredTimescale: 1), queue: DispatchQueue.main) { time in
-//            print(self.player?.currentItem?.status)
-//            if self.player?.currentItem?.status == .readyToPlay {
-//
-//            }
-//        }
+    static func onError(_ error: Error) {
+        print(error)
     }
+    
+    func resetQueue() {
+        player.resetQueue(new: Global.current.dataFlowController.currentState.state.tracks)
+    }
+    
+//    func update() {
+//        guard let track = Global.current.dataFlowController.currentState.state.tracks.first else { return }
+//        songTitleLabel.stringValue = track.title
+//        artistAndAlbumLabel.stringValue = "\(track.album) (\(track.artist))"
+//    }
     
     deinit {
         print("PlayerController deinit")
