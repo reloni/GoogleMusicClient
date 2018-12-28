673949fc4ddbdd1322bad05dd07caad549ddf5a5
Anton Efimenko <reloni@ya.ru>
2018-12-28 15:46:52 +0300

    Show player queue in controller

--- GoogleMusicClientMacOS/Controllers/Queue/QueueController.swift
+++ GoogleMusicClientMacOS/Controllers/Queue/QueueController.swift
@@ -7,9 +7,39 @@
 //
 
 import Cocoa
+import RxGoogleMusic
 
 final class QueueController: NSViewController {
-//    static func instantiate() -> QueueController {
-//        return storyboard.instantiateController(withIdentifier: "123") as! QueueController
+    @IBOutlet weak var tableView: ApplicationTableView!
+    
+    var queue: [GMusicTrack] {
+        return Global.current.dataFlowController.currentState.state.player?.items ?? []
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension QueueController: NSTableViewDataSource {
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return queue.count
+    }
+}
+
+extension QueueController: NSTableViewDelegate {
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "Cell"), owner: nil) as! NSTableCellView
+        cell.textField?.stringValue = queue[row].title
+        return cell
+    }
+    
+//    func tableViewSelectionDidChange(_ notification: Notification) {
+//        guard 0..<stations.count ~= tableView.selectedRow else { return }
+//        let station = stations[tableView.selectedRow]
+//        Global.current.dataFlowController.dispatch(PlayerAction.loadRadioStationFeed(station))
 //    }
 }
