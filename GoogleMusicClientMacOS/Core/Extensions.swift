11fcd1e0accb383e9aff95d5ffa74c624f44e97b
Anton Efimenko <reloni@ya.ru>
2018-12-29 10:18:20 +0300

    Save window position

--- GoogleMusicClientMacOS/Core/Extensions.swift
+++ GoogleMusicClientMacOS/Core/Extensions.swift
@@ -9,6 +9,69 @@
 import Foundation
 import Cocoa
 
+enum WindowSize: Codable {
+    case fullScreen
+    case rect(NSRect)
+    
+    enum CodingKeys: String, CodingKey {
+        case type
+        case width
+        case height
+        case x
+        case y
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let type = try container.decode(String.self, forKey: .type)
+        switch type {
+        case "fullScreen": self = .fullScreen
+        case "rect":
+            let width: CGFloat = try container.decode(.width)
+            let height: CGFloat = try container.decode(.height)
+            let x: CGFloat = try container.decode(.x)
+            let y: CGFloat = try container.decode(.y)
+            self = .rect(NSRect(x: x, y: y, width: width, height: height))
+        default:
+            throw DecodingError.dataCorruptedError(forKey: .type, in: container, debugDescription: "Unable to deserialize WindowSize")
+        }
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case .fullScreen:
+            try container.encode("fullScreen", forKey: .type)
+        case .rect(let rect):
+            try container.encode("rect", forKey: .type)
+            try container.encode(rect.height, forKey: .height)
+            try container.encode(rect.width, forKey: .width)
+            try container.encode(rect.origin.x, forKey: .x)
+            try container.encode(rect.origin.y, forKey: .y)
+        }
+    }
+}
+
+extension UserDefaults {
+    var mainWindowFrame: WindowSize? {
+        set {
+            guard let size = newValue else {
+                set(nil, forKey: "mainWindowFrame")
+                return
+            }
+            let data = try? JSONEncoder().encode(size)
+            set(data, forKey: "mainWindowFrame")
+        }
+        get {
+            guard let data = self.data(forKey: "mainWindowFrame") else {
+                return nil
+            }
+            return try? JSONDecoder().decode(WindowSize.self, from: data)
+        }
+    }
+}
+
 extension TimeInterval {
     var timeString: String? {
         guard !isInfinite, !isNaN else { return nil }
